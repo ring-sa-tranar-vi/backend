873370ab7f9d3aa49eba7532d1831efb673ac1c9
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-03 14:34:36 +0200

    Add a basic controller to say Hello

--- src/main/java/dev/salt/Ring20/controller/HiController.java
+++ src/main/java/dev/salt/Ring20/controller/HiController.java
@@ -1,4 +1,24 @@
 package dev.salt.Ring20.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class HiController {
+
+    private final HiService hiService;
+
+    public HiController(HiService hiservice) {
+        this.hiService = hiservice;
+    }
+
+    @GetMapping("/hi")
+    public ResponseEntity<String> sayHi(){
+        return ResponseEntity.ok("");
+    }
 }
