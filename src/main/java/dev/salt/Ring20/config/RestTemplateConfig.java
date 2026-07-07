861ef8774cc15ed6494ae95de01fba3691ae862a
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-07 16:51:51 +0200

    Switch back to local database h2

--- src/main/java/dev/salt/Ring20/config/RestTemplateConfig.java
+++ src/main/java/dev/salt/Ring20/config/RestTemplateConfig.java
@@ -1,7 +1,9 @@
 package dev.salt.Ring20.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,5 +12,15 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
