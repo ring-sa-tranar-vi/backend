401c9717ff8f7f9fac459a06eee3e4c07f4f5368
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-14 16:29:05 +0200

    Add basic controller

---
+++ src/main/java/dev/salt/Ring20/controller/OrganisationController.java
@@ -0,0 +1,30 @@
+package dev.salt.Ring20.controller;
+
+import dev.salt.Ring20.entity.Organisation;
+import dev.salt.Ring20.service.OrganisationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api/organisations")
+@CrossOrigin(origins = {"http://localhost:5173", "https://https://prod-ringsatranarvi-app.web.app/"})
+public class OrganisationController {
+    private OrganisationService service;
+
+//    @PostMapping
+//    private ResponseEntity<Organisation> createOrganisation(){
+//
+//    }
+//    @GetMapping
+//    private ResponseEntity<Organisation>  getAllOrganisations(){}
+//    @GetMapping
+//    private ResponseEntity<Organisation> getOrganisationById(){}
+//
+//    @DeleteMapping
+//    private ResponseEntity<Organisation> deleteOrganisation(){}
+//
+//    @PutMapping
+//    private ResponseEntity<Organisation> updateOrganisation(){}
+
+}
---
+++ src/main/java/dev/salt/Ring20/service/OrganisationService.java
@@ -0,0 +1,4 @@
+package dev.salt.Ring20.service;
+
+public class OrganisationService {
+}
