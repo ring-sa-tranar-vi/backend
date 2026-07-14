c759d111889e1a6e04d29a3180922a880de3e4d3
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-14 16:29:28 +0200

    Add basic organisation service

--- src/main/java/dev/salt/Ring20/service/OrganisationService.java
+++ src/main/java/dev/salt/Ring20/service/OrganisationService.java
@@ -1,4 +1,10 @@
 package dev.salt.Ring20.service;
 
+import dev.salt.Ring20.repository.OrganisationRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrganisationService {
+    private OrganisationRepository repo;
+
 }
