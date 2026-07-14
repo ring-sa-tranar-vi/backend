37b92a23f10346698d12da145427201d15fe4639
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-14 14:04:37 +0200

    Add organisation entity

---
+++ src/main/java/dev/salt/Ring20/entity/Organisation.java
@@ -0,0 +1,18 @@
+package dev.salt.Ring20.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Organisation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private List<String> events;
+}
