5fe7739a762a57db0772dc5591bdba8a638fc5ee
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-14 16:04:13 +0200

    Add organisation repo interface

---
+++ src/main/java/dev/salt/Ring20/repository/OrganisationRepository.java
@@ -0,0 +1,7 @@
+package dev.salt.Ring20.repository;
+
+import dev.salt.Ring20.entity.Organisation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganisationRepository extends JpaRepository<Organisation, Long> {
+}
