be72196f28e0e0cea338f32ab4cd85abc63467d3
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-14 16:46:59 +0200

    Add add organisation method in service

--- src/main/java/dev/salt/Ring20/service/OrganisationService.java
+++ src/main/java/dev/salt/Ring20/service/OrganisationService.java
@@ -1,10 +1,22 @@
 package dev.salt.Ring20.service;
 
+import dev.salt.Ring20.entity.Event;
+import dev.salt.Ring20.entity.Organisation;
 import dev.salt.Ring20.repository.OrganisationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrganisationService {
     private OrganisationRepository repo;
 
+    public OrganisationService(OrganisationRepository repo) {
+        this.repo = repo;
+    }
+
+    public Organisation createOrganisation(String name, String description, List<Event> events){
+        Organisation newOrg = new Organisation(name, description, events);
+        return repo.save(newOrg);
+    }
 }
