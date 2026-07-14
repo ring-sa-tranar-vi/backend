a132ad6a40361348ccd88596492d1536edb98a9c
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-14 16:05:42 +0200

    Add event repo interface

--- src/main/java/dev/salt/Ring20/repository/Event.java
+++ src/main/java/dev/salt/Ring20/repository/Event.java
@@ -1,9 +1,8 @@
 package dev.salt.Ring20.repository;
 
-import dev.salt.Ring20.entity.Organisation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrganisationRepository extends JpaRepository<Organisation, Long> {
+public interface Event extends JpaRepository<Event, Long> {
 }
