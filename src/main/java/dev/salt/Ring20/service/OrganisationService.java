7e5f4a7f72c8fb623feff669c4a09062627f8c6c
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-14 17:02:35 +0200

    Implement post, delete, get and update for organisation in service

--- src/main/java/dev/salt/Ring20/service/OrganisationService.java
+++ src/main/java/dev/salt/Ring20/service/OrganisationService.java
@@ -4,12 +4,13 @@ import dev.salt.Ring20.entity.Event;
 import dev.salt.Ring20.entity.Organisation;
 import dev.salt.Ring20.repository.OrganisationRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class OrganisationService {
-    private OrganisationRepository repo;
+    private final OrganisationRepository repo;
 
     public OrganisationService(OrganisationRepository repo) {
         this.repo = repo;
@@ -19,4 +20,24 @@ public class OrganisationService {
         Organisation newOrg = new Organisation(name, description, events);
         return repo.save(newOrg);
     }
+    public List<Organisation> getAllOrganisations(){
+        return repo.findAll();
+    }
+    public Organisation getOrganisationById(Long id){
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Organisation not found with id: " + id));
+    }
+    public void deleteOrganisationById(Long id){
+        repo.deleteById(id);
+    }
+    @Transactional
+    public Organisation updateOrganisationById(Long id, String name, String description, List<Event> events){
+        Organisation foundOrg = repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Organisation not found with id: " + id));
+        foundOrg.setName(name);
+        foundOrg.setDescription(description);
+        foundOrg.setEvents(events);
+        return repo.save(foundOrg);
+    }
+
 }
