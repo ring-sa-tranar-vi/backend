1c3ba313829a66f005536bf73292941edd02840c
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-15 11:47:32 +0200

    Add organisation request dto

---
+++ src/main/java/dev/salt/Ring20/dto/OrganisationResponseDTO.java
@@ -0,0 +1,13 @@
+package dev.salt.Ring20.dto;
+
+import dev.salt.Ring20.entity.Event;
+
+import java.util.List;
+
+public record OrganisationResponseDTO(
+        Long id,
+        String name,
+        String description,
+        List<Event> events
+) {
+}
