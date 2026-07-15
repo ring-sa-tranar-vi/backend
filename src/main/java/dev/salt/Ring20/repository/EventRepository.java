8f66b9d5b0ef3a1c652c72588aded0e833858e88
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-15 16:20:46 +0200

    Add find by organisation ID

--- src/main/java/dev/salt/Ring20/repository/EventRepository.java
+++ src/main/java/dev/salt/Ring20/repository/EventRepository.java
@@ -3,5 +3,8 @@ package dev.salt.Ring20.repository;
 import dev.salt.Ring20.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByOrganisationId(Long organisationId);
 }
