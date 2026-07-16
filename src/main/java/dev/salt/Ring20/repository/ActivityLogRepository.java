64422e8ac410d114cc1f1ced07d9403d521f1271
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-16 17:00:38 +0200

    Clean up in activity log repo

--- src/main/java/dev/salt/Ring20/repository/ActivityLogRepository.java
+++ src/main/java/dev/salt/Ring20/repository/ActivityLogRepository.java
@@ -1,9 +1,11 @@
 package dev.salt.Ring20.repository;
 
 import dev.salt.Ring20.entity.ActivityLog;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
