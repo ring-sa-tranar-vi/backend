d49856ce639775cb6b54982c7074196dde06b1c0
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-16 17:03:49 +0200

    Clean up spaces in user and userworkout repo

--- src/main/java/dev/salt/Ring20/repository/UserRepository.java
+++ src/main/java/dev/salt/Ring20/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package dev.salt.Ring20.repository;
 
 import dev.salt.Ring20.entity.User;
+
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
