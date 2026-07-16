7b7500232b4aec3c0b2170f45c33d32c3d3259f7
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-16 17:02:48 +0200

    Clean up spaces in org repo

--- src/main/java/dev/salt/Ring20/repository/OrganisationRepository.java
+++ src/main/java/dev/salt/Ring20/repository/OrganisationRepository.java
@@ -3,4 +3,5 @@ package dev.salt.Ring20.repository;
 import dev.salt.Ring20.entity.Organisation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrganisationRepository extends JpaRepository<Organisation, Long> {}
+public interface OrganisationRepository extends JpaRepository<Organisation, Long> {
+}
