76ce3388c8cca1e1ffffea4ecb7c77f279459cce
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-16 17:04:53 +0200

    Clean up workout repo and remove unused method

--- src/main/java/dev/salt/Ring20/repository/WorkoutRepository.java
+++ src/main/java/dev/salt/Ring20/repository/WorkoutRepository.java
@@ -1,12 +1,12 @@
 package dev.salt.Ring20.repository;
 
 import dev.salt.Ring20.entity.Workout;
+
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
-    List<Workout> findByTrainerId(Long trainerId);
-
     List<Workout> findByEnabledTrue();
 
     List<Workout> findByTrainerIdAndEnabledTrue(Long trainerId);
