a186f7e9fda691261d19ccebd4a253fc70a2e1a1
Mattias Söderberg <mattes.email@gmail.com>
2026-07-21 17:05:49 +0200

    Add GcsStorageService for Google Cloud Storage file access

---
+++ src/main/java/dev/salt/Ring20/service/GcsStorageService.java
@@ -0,0 +1,36 @@
+package dev.salt.Ring20.service;
+
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Profile("!local")
+public class GcsStorageService implements FileStorageService {
+
+    private final Storage storage;
+    private final String bucketName;
+
+    public GcsStorageService(@Value("${gcp.storage.bucket-name}") String bucketName) {
+        storage = StorageOptions.getDefaultInstance().getService();
+        this.bucketName = bucketName;
+        System.out.println("GcsStorageService initialized with bucket name: " + bucketName);
+    }
+
+    @Override
+    public String getFileAccess(String filePath, int validForMinutes) {
+        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, filePath).build();
+        URL signedUrl = storage.signUrl(
+                blobInfo, validForMinutes, TimeUnit.MINUTES,
+                Storage.SignUrlOption.withV4Signature()
+        );
+
+        return signedUrl.toString();
+    }
+}
