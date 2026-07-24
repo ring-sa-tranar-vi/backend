c1f6f40e8c3a25b30df592d3d032a12966c248c3
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-24 11:14:29 +0200

    Add security service to check user role

--- src/main/java/dev/salt/Ring20/service/SecurityService.java
+++ src/main/java/dev/salt/Ring20/service/SecurityService.java
@@ -1,4 +1,26 @@
 package dev.salt.Ring20.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SecurityService {
+    private final UserService userService;
+
+    public SecurityService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public boolean isCurrentUser(Long userId, String clerkId) {
+        Long currentUserId = userService.getInternalUserId(clerkId);
+        return currentUserId.equals(userId);
+    }
+
+    public boolean isAdmin(String clerkId) {
+        return userService.isAdmin(clerkId);
+    }
+
+    public boolean isOwnerOrAdmin(Long userId, String clerkId) {
+        return isCurrentUser(userId, clerkId)
+                || isAdmin(clerkId);
+    }
 }
