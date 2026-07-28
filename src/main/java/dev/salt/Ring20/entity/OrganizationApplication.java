0cb7985a7bc79fcbbd4431f99b96bfc7bc98cbf5
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-28 11:44:20 +0200

    Add organisation application entity and enum

---
+++ src/main/java/dev/salt/Ring20/entity/OrganizationApplication.java
@@ -0,0 +1,37 @@
+package dev.salt.Ring20.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrganizationApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private User user;
+
+    private String organizationName;
+
+    @Column(length = 2000)
+    private String description;
+
+    @Column(length = 2000)
+    private String motivation;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime reviewedAt;
+
+
+}
