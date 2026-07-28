be074a2513a84acbf1d7ca226f369a4413d45c6c
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-28 11:52:15 +0200

    Add basic controller and service for organization application

---
+++ src/main/java/dev/salt/Ring20/controller/OrganizationApplicationController.java
@@ -0,0 +1,11 @@
+package dev.salt.Ring20.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/organization-applications")
+public class OrganizationApplicationController {
+
+
+}
---
+++ src/main/java/dev/salt/Ring20/service/OrganizationApplicationService.java
@@ -0,0 +1,7 @@
+package dev.salt.Ring20.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrganizationApplicationService {
+}
