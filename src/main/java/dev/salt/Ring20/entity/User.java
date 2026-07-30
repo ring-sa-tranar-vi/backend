1a14f9bf3e37d3e6da325c1fe9fbbeac1d58ad91
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-30 12:11:11 +0200

    Add fcm string token to user entity

--- src/main/java/dev/salt/Ring20/entity/User.java
+++ src/main/java/dev/salt/Ring20/entity/User.java
@@ -1,8 +1,10 @@
 package dev.salt.Ring20.entity;
 
 import jakarta.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -45,6 +47,7 @@ public class User {
     @ManyToMany
     @JoinTable(name = "user_events")
     private List<Event> attendingEvents = new ArrayList<>();
+    private String fcmToken;
 
     public User(String name, Integer intensityLevel, String context, String clerkId) {
         this.name = name;
