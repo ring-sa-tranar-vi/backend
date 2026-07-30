f4661e469cf7166c4bd6308ee135628ea1b32c22
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-30 15:40:05 +0200

    Fix linting

--- src/main/java/dev/salt/Ring20/config/FirebaseConfig.java
+++ src/main/java/dev/salt/Ring20/config/FirebaseConfig.java
@@ -4,11 +4,8 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
