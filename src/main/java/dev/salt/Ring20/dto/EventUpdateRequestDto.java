353a71b48733ca937f1fcca608ccd5884311d3cd
Tehreem Asif <tehreem.asif63@gmail.com>
2026-07-31 17:37:16 +0200

    Separate event create request dto into two one for create and one for update with update not containing organiser id and fixed methods

--- src/main/java/dev/salt/Ring20/dto/EventUpdateRequestDto.java
+++ src/main/java/dev/salt/Ring20/dto/EventUpdateRequestDto.java
@@ -1,16 +1,19 @@
 package dev.salt.Ring20.dto;
 
 import dev.salt.Ring20.entity.EventType;
-import dev.salt.Ring20.entity.Organisation;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
-public record EventRequestDto(
-        @NotBlank String name,
+public record EventUpdateRequestDto (
+        @NotBlank
+        String name,
         String description,
-        @NotNull LocalDateTime time,
-        @NotNull Long organisationId,
+        @NotNull
+        LocalDateTime time,
         @NotBlank String city,
         String venue,
-        @NotNull EventType eventType) {}
+        @NotNull
+        EventType eventType
+){}
