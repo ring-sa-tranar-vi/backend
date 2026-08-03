3618029dcb6a9c6aa9856ca6e6e1eaca0aeb0d9d
Tehreem Asif <tehreem.asif63@gmail.com>
2026-08-03 12:44:28 +0200

    Fix lazy loading issue of events and org

--- src/main/java/dev/salt/Ring20/repository/EventRepository.java
+++ src/main/java/dev/salt/Ring20/repository/EventRepository.java
@@ -2,12 +2,22 @@ package dev.salt.Ring20.repository;
 
 import dev.salt.Ring20.entity.Event;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     @Query("SELECT event FROM Event event JOIN FETCH event.organisation")
     List<Event> findAllWithOrganisation();
 
     List<Event> findByOrganisationId(Long organisationId);
+    @Query("""
+       SELECT e
+       FROM Event e
+       LEFT JOIN FETCH e.organisation
+       WHERE e.id = :id
+       """)
+    Optional<Event> findByIdWithOrganisation(@Param("id") Long id);
 }
