f45c41c2513f58af716d7d66b717e34267f73a49
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-08-03 17:09:07 +0200

    Refactor of constants into field

--- src/main/java/dev/salt/Ring20/config/LocalStorageWebConfig.java
+++ src/main/java/dev/salt/Ring20/config/LocalStorageWebConfig.java
@@ -9,12 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Profile("local")
 public class LocalStorageWebConfig implements WebMvcConfigurer {
 
-    //TODO: constants need to be separate, this will make it easies to edit and understand later.
-    // in this case: file:./ringsatranarvi-files/ -> this could be another name and could be then easily changed
+    private final static String PATH_PATTERN = "/local-storage/**";
+    private final static String LOCATION = "file:./ringsatranarvi_files/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/local-storage/**")
-                .addResourceLocations("file:./ringsatranarvi_files/");
+        registry.addResourceHandler(PATH_PATTERN)
+                .addResourceLocations(LOCATION);
     }
 }
