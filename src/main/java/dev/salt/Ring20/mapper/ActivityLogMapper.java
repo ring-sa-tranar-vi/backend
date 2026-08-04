677e00dda6edba37506a10a84c03b4f41c741d06
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-08-04 16:19:49 +0200

    Add Mapper to remove mapping toEntity and toResponse fdrom Controller

---
+++ src/main/java/dev/salt/Ring20/mapper/ActivityLogMapper.java
@@ -0,0 +1,29 @@
+package dev.salt.Ring20.mapper;
+
+import dev.salt.Ring20.dto.activityLog.ActivityLogCreateRequestDto;
+import dev.salt.Ring20.dto.activityLog.ActivityLogResponseDto;
+import dev.salt.Ring20.entity.ActivityLog;
+
+public class ActivityLogMapper {
+    public static ActivityLog toEntity(ActivityLogCreateRequestDto request) {
+        ActivityLog activityLog = new ActivityLog();
+        activityLog.setWorkoutId(request.workoutId());
+        activityLog.setCompletedAt(request.completedAt());
+        activityLog.setDurationSeconds(request.durationSeconds());
+        activityLog.setFeedback(request.feedback());
+        activityLog.setStatus(request.status());
+        return activityLog;
+    }
+
+    public static ActivityLogResponseDto toResponse(ActivityLog activityLog) {
+        return new ActivityLogResponseDto(
+                activityLog.getId(),
+                activityLog.getUserId(),
+                activityLog.getWorkoutId(),
+                activityLog.getCompletedAt(),
+                activityLog.getDurationSeconds(),
+                activityLog.getFeedback(),
+                activityLog.getStatus());
+    }
+
+}
