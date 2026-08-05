b190723a53dfe77ad90d4df3a81410bbc6a844c8
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-08-05 14:55:30 +0200

    Remove comment

--- src/main/java/dev/salt/Ring20/dto/calendarEventDtos/CalendarEventDto.java
+++ src/main/java/dev/salt/Ring20/dto/calendarEventDtos/CalendarEventDto.java
@@ -1,11 +1,12 @@
 package dev.salt.Ring20.dto.calendarEventDtos;
 
 import java.time.LocalDateTime;
-// TODO exist calendarEventType enum, why not use it?
+
 public record CalendarEventDto(
         String id,
         String type,
         String title,
         String description,
         LocalDateTime time,
-        boolean completed) {}
+        boolean completed) {
+}
