7742d6718bd60024c4eb34ebd9a95e8753fec7c6
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-08-05 16:12:51 +0200

    Remove duplicate dtos

--- src/main/java/dev/salt/Ring20/dto/companyDtos/CompanyMeResponseDto.java
+++
@@ -1,8 +0,0 @@
-package dev.salt.Ring20.dto.companyDtos;
-
-public record CompanyMeResponseDto(
-        Long userId,
-        String role,
-        boolean canManageOrganisation,
-        Long organisationId,
-        String organisationName) {}
