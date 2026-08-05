b312f1fb531753c1709f0e12a189b0d44a575a6d
Sebastian Thunberg <sebastian.thunberg@appliedtechnology.se>
2026-08-05 16:37:50 +0200

    Removed authentication

--- src/main/java/dev/salt/Ring20/controller/TrainerController.java
+++ src/main/java/dev/salt/Ring20/controller/TrainerController.java
@@ -42,7 +42,6 @@ public class TrainerController {
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("isAuthenticated()")
     @Operation(summary = "Get trainer by ID", description = "Retrieves a trainer using their ID.")
     public ResponseEntity<TrainerResponseDto> getTrainerById(@PathVariable Long id) {
         Trainer trainer = trainerService.getTrainerById(id);
