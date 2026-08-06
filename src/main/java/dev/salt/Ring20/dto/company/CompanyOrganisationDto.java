2d9db88c501923e24d54f10e761fb7d452f271af
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-08-06 16:02:41 +0200

    refactor styling

--- src/main/java/dev/salt/Ring20/dto/company/CompanyOrganisationDto.java
+++ src/main/java/dev/salt/Ring20/dto/company/CompanyOrganisationDto.java
@@ -1,3 +1,6 @@
 package dev.salt.Ring20.dto.company;
 
-public record CompanyOrganisationDto(Long id, String name, String description, String orgCity) {}
+public record CompanyOrganisationDto(Long id,
+                                     String name,
+                                     String description,
+                                     String orgCity) {}
