e80b471296da430f51f042fc9a865db4ad2107a0
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-08-06 16:03:06 +0200

    refactor styling

--- src/main/java/dev/salt/Ring20/dto/UpdateCompanyOrganisationDto.java
+++ src/main/java/dev/salt/Ring20/dto/UpdateCompanyOrganisationDto.java
@@ -3,4 +3,7 @@ package dev.salt.Ring20.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpdateCompanyOrganisationDto(
-        @NotBlank String name, String description, @NotBlank String orgCity) {}
+        @NotBlank String name,
+        String description,
+        @NotBlank String orgCity) {
+}
