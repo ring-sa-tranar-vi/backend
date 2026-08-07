7f72e750c7120550744d18b0d170a70c88564ad8
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-08-07 17:22:23 +0200

    update mapper methods

--- src/main/java/dev/salt/Ring20/mapper/TrainerMapper.java
+++ src/main/java/dev/salt/Ring20/mapper/TrainerMapper.java
@@ -4,9 +4,9 @@ import dev.salt.Ring20.dto.trainerDtos.TrainerRequestDto;
 import dev.salt.Ring20.dto.trainerDtos.TrainerResponseDto;
 import dev.salt.Ring20.dto.workoutDtos.RecommendWorkoutResponseDto;
 import dev.salt.Ring20.entity.Trainer;
-import dev.salt.Ring20.service.FileStorageService;
 import dev.salt.Ring20.service.data.RecommendedWorkoutData;
 import dev.salt.Ring20.service.data.TrainerData;
+import dev.salt.Ring20.service.storage.FileStorageService;
 
 public class TrainerMapper {
     public static TrainerData toTrainerData(TrainerRequestDto request) {
@@ -22,18 +22,22 @@ public class TrainerMapper {
                 request.ambience());
     }
 
-    public static RecommendWorkoutResponseDto toRecommendedWorkoutResponse(RecommendedWorkoutData data) {
+    public static RecommendWorkoutResponseDto toRecommendedWorkoutResponse(
+            RecommendedWorkoutData data) {
         return new RecommendWorkoutResponseDto(data.workoutId(), data.reasoning());
     }
 
-    public static TrainerResponseDto toResponseDto(Trainer trainer, FileStorageService fileStorageService, int validMinutes) {
+    public static TrainerResponseDto toResponseDto(
+            Trainer trainer, FileStorageService fileStorageService, int validMinutes) {
         String introUrl = getFileUrl(trainer.getIntro(), fileStorageService, validMinutes);
 
-        String imageSelectUrl = getFileUrl(trainer.getImageSelect(), fileStorageService, validMinutes);
+        String imageSelectUrl =
+                getFileUrl(trainer.getImageSelect(), fileStorageService, validMinutes);
 
         String imageCallUrl = getFileUrl(trainer.getImageCall(), fileStorageService, validMinutes);
 
-        String imageStartUrl = getFileUrl(trainer.getImageStart(), fileStorageService, validMinutes);
+        String imageStartUrl =
+                getFileUrl(trainer.getImageStart(), fileStorageService, validMinutes);
 
         return new TrainerResponseDto(
                 trainer.getId(),
@@ -49,13 +53,8 @@ public class TrainerMapper {
     }
 
     private static String getFileUrl(
-            String file,
-            FileStorageService fileStorageService,
-            int validMinutes) {
+            String file, FileStorageService fileStorageService, int validMinutes) {
 
-        return file != null
-                ? fileStorageService.getFileAccess(file, validMinutes)
-                : null;
+        return file != null ? fileStorageService.getFileAccess(file, validMinutes) : null;
     }
-
 }
