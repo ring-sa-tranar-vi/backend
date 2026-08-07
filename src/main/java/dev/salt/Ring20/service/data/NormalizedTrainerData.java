87e99590c94e5d28db7a5c8238cfb1c04fcfbafb
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-08-07 13:45:39 +0200

    Add NormalizedTrainerData to remove duplicated code

---
+++ src/main/java/dev/salt/Ring20/service/data/NormalizedTrainerData.java
@@ -0,0 +1,8 @@
+package dev.salt.Ring20.service.data;
+
+public record NormalizedTrainerData( String name,
+                                     String prompt,
+                                     String voice,
+                                     String intro,
+                                     String language) {
+}
