ea3ac037d49d3ce84f8fff87f08f8de50b5f08bf
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-08-07 11:51:27 +0200

    Add new mapper method

--- src/main/java/dev/salt/Ring20/mapper/TrainerMapper.java
+++ src/main/java/dev/salt/Ring20/mapper/TrainerMapper.java
@@ -1,7 +1,10 @@
 package dev.salt.Ring20.mapper;
 
 import dev.salt.Ring20.dto.trainerDtos.TrainerRequestDto;
+import dev.salt.Ring20.dto.trainerDtos.TrainerResponseDto;
 import dev.salt.Ring20.dto.workoutDtos.RecommendWorkoutResponseDto;
+import dev.salt.Ring20.entity.Trainer;
+import dev.salt.Ring20.service.FileStorageService;
 import dev.salt.Ring20.service.data.RecommendedWorkoutData;
 import dev.salt.Ring20.service.data.TrainerData;
 
@@ -21,6 +24,38 @@ public class TrainerMapper {
 
     public static RecommendWorkoutResponseDto toRecommendedWorkoutResponse(RecommendedWorkoutData data) {
         return new RecommendWorkoutResponseDto(data.workoutId(), data.reasoning());
+    }
+
+    public static TrainerResponseDto toResponseDto(Trainer trainer, FileStorageService fileStorageService, int validMinutes) {
+        String introUrl = getFileUrl(trainer.getIntro(), fileStorageService, validMinutes);
+
+        String imageSelectUrl = getFileUrl(trainer.getImageSelect(), fileStorageService, validMinutes);
+
+        String imageCallUrl = getFileUrl(trainer.getImageCall(), fileStorageService, validMinutes);
+
+        String imageStartUrl = getFileUrl(trainer.getImageStart(), fileStorageService, validMinutes);
+
+        return new TrainerResponseDto(
+                trainer.getId(),
+                trainer.getName(),
+                trainer.getPrompt(),
+                trainer.getVoice(),
+                introUrl,
+                trainer.getLanguage(),
+                imageSelectUrl,
+                imageCallUrl,
+                imageStartUrl,
+                trainer.getAmbience());
+    }
+
+    private static String getFileUrl(
+            String file,
+            FileStorageService fileStorageService,
+            int validMinutes) {
+
+        return file != null
+                ? fileStorageService.getFileAccess(file, validMinutes)
+                : null;
     }
 
 }
