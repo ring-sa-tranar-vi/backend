fa3d6cedaefcf092584dff18548a0851f0dd1152
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2026-08-07 17:50:40 +0200

    refactor for consistency

--- src/main/java/dev/salt/Ring20/service/security/ActivityLogSecurity.java
+++ src/main/java/dev/salt/Ring20/service/security/ActivityLogSecurity.java
@@ -4,14 +4,13 @@ import dev.salt.Ring20.entity.ActivityLog;
 import dev.salt.Ring20.repository.ActivityLogRepository;
 import org.springframework.stereotype.Service;
 
-// TODO: not consistent as with the SecurityService
-@Service
-public class ActivityLogSecurityService {
+@Service("activityLogSecurity")
+public class ActivityLogSecurity {
 
     private final ActivityLogRepository repository;
     private final SecurityService securityService;
 
-    public ActivityLogSecurityService(
+    public ActivityLogSecurity(
             ActivityLogRepository repository, SecurityService securityService) {
         this.repository = repository;
         this.securityService = securityService;
