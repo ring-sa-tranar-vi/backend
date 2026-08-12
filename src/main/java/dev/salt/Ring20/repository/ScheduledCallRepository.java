101638dc28c35b988ec6e4e92994a3931a8018e1
Tehreem Asif <tehreem.asif63@gmail.com>
2026-08-12 15:23:11 +0200

    Add timestamp so detash historic calls actually only detach the past calls

--- src/main/java/dev/salt/Ring20/repository/ScheduledCallRepository.java
+++ src/main/java/dev/salt/Ring20/repository/ScheduledCallRepository.java
@@ -26,8 +26,10 @@ public interface ScheduledCallRepository extends JpaRepository<ScheduledCall, Lo
                 UPDATE ScheduledCall c
                 SET c.callbackPreference = NULL
                 WHERE c.callbackPreference.id = :preferenceId
+                  AND c.targetTime < CURRENT_TIMESTAMP
             """)
-    void detachPreferenceFromHistoricalCalls(@Param("preferenceId") Long preferenceId);
+    void detachPreferenceFromHistoricalCalls(
+            @Param("preferenceId") Long preferenceId);
 
     @Modifying
     @Query(
