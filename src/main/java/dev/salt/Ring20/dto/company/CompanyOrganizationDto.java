aefa96055189fb2189f43af544251db8ef2e118a
Tehreem Asif <tehreem.asif63@gmail.com>
2026-08-13 13:47:11 +0200

    Add follower count in the company org dto

--- src/main/java/dev/salt/Ring20/dto/company/CompanyOrganizationDto.java
+++ src/main/java/dev/salt/Ring20/dto/company/CompanyOrganizationDto.java
@@ -1,3 +1,4 @@
 package dev.salt.Ring20.dto.company;
 
-public record CompanyOrganizationDto(Long id, String name, String description, String orgCity) {}
+public record CompanyOrganizationDto(Long id, String name, String description, String orgCity, int followerCount) {
+}
